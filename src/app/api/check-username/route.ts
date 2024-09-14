25ab897f25874625b27a1a4bf19ced089eb67099
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-14 21:42:59 +0530

    check-username api created

---
+++ src/app/api/check-username/route.ts
@@ -0,0 +1,79 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+const usernameQuerySchema = z.object({
+    username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+    await dbConnect();
+
+    try {
+        const { searchParams } = new URL(request.url);
+        const queryParam = {
+            username: searchParams.get("username"),
+        };
+
+        //validate with zod
+        const result = usernameQuerySchema.safeParse(queryParam);
+        console.log(result);
+        if (!result.success) {
+            const usernameErrors =
+                result.error.format().username?._errors || [];
+            return Response.json(
+                {
+                    success: false,
+                    message:
+                        usernameErrors?.length > 0
+                            ? usernameErrors.join(", ")
+                            : "Invalid quey parameters",
+                },
+                {
+                    status: 400,
+                }
+            );
+        }
+
+        const { username } = result.data;
+
+        const existingVerifiedUser = await UserModel.findOne({
+            username,
+            isVerified: true,
+        });
+
+        if (existingVerifiedUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Username is already taken",
+                },
+                {
+                    status: 400,
+                }
+            );
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "Username is unique",
+            },
+            {
+                status: 200,
+            }
+        );
+    } catch (err) {
+        console.error();
+        return Response.json(
+            {
+                success: false,
+                message: "Error hecking username",
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+}
