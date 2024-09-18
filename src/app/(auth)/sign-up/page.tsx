f73e9be119d58e05fb0a99305d8eaadc4f9e9d7f
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-18 21:18:35 +0530

    Sign up message fix

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -174,7 +174,7 @@ function SignUp() {
                 </Form>
                 <div className="text-center mt-4">
                     <p>
-                        Not a member yet?{" "}
+                        Already a member ?{" "}
                         <Link
                             href="/sign-in"
                             className="text-blue-600 hover:text-blue-800"
