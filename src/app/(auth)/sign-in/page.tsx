0de5e289f956fb01c861fb24a323f26d110634b2
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-18 21:18:42 +0530

    Sign in implemented

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,128 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { signInSchema } from "@/schemas/signInSchema";
+import {
+    Form,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { signIn } from "next-auth/react";
 
 function SignIn() {
-    return <div>SignIn</div>;
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const { toast } = useToast();
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof signInSchema>>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            identifier: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+        setIsSubmitting(true);
+        const result = await signIn("credentials", {
+            identifier: data.identifier,
+            password: data.password,
+            redirect: false,
+        });
+        setIsSubmitting(false);
+        if (result?.error) {
+            toast({
+                variant: "destructive",
+                title: "Login failed",
+                description: "Incorrect username or password",
+            });
+        }
+
+        if (result?.url) {
+            router.replace("/dashboard");
+        }
+    };
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-brown-800">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+                <div className="text-center">
+                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6 text-brown-500">
+                        Join Incognito Msg
+                    </h1>
+                    <p className="mb-4">
+                        Sign in to start your anonymous adventure
+                    </p>
+                </div>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-6"
+                    >
+                        <FormField
+                            name="identifier"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username/Email</FormLabel>
+                                    <Input
+                                        placeholder="Enter username or email"
+                                        {...field}
+                                    />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="password"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <Input
+                                        type="password"
+                                        placeholder="********"
+                                        {...field}
+                                    />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" disabled={isSubmitting}>
+                            {isSubmitting ? (
+                                <>
+                                    <Loader2 className="m2-2 h-4 w-4 animate-spin" />{" "}
+                                    Please wait
+                                </>
+                            ) : (
+                                "Signin"
+                            )}
+                        </Button>
+                    </form>
+                </Form>
+                <div className="text-center mt-4">
+                    <p>
+                        Not a member yet?{" "}
+                        <Link
+                            href="/sign-up"
+                            className="text-blue-600 hover:text-blue-800"
+                        >
+                            Sign up
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default SignIn;
