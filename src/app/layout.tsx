373a10463d4e9a12a9f3a8a96f772541b6045570
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-19 20:58:37 +0530

    navbr improved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster";
 import NavBar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,10 +20,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <NavBar />
-                {children}
-            </body>
+            <AuthProvider>
+                <body className={inter.className}>
+                    <NavBar />
+                    {children}
+                    <Toaster />
+                </body>
+            </AuthProvider>
         </html>
     );
 }
