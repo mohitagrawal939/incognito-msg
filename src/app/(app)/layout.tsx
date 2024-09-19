1c42174edb66df925ed58ccd7df35e18de6c8b03
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-19 23:43:22 +0530

    Navbar fixed

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,26 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import NavBar from "@/components/NavBar";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-    title: "Incognito-msg",
-    description:
-        "Simple anonymous message application with Mongoose and NextJS in TypeScript.",
-};
-
-export default function RootLayout({
-    children,
-}: Readonly<{
+interface RootLayoutProps {
     children: React.ReactNode;
-}>) {
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
     return (
-        <html lang="en">
-            <body className={inter.className}>
-                <NavBar />
-                {children}
-            </body>
-        </html>
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            {children}
+        </div>
     );
 }
