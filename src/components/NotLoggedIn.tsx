3247c009982e562f85386566aba74e76b918fc87
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-21 02:29:12 +0530

    login corrected

--- src/components/NotLoggedIn.tsx
+++ src/components/NotLoggedIn.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function NotLoggedIn() {
@@ -6,7 +7,13 @@ function NotLoggedIn() {
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <div className="text-6xl">
-                    Please login from<a href="/sign-in">here</a>
+                    Please login from{" "}
+                    <Link
+                        href="/sign-in"
+                        className="text-blue-600 hover:text-blue-800"
+                    >
+                        here
+                    </Link>
                 </div>
             </main>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
