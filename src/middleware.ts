c62c1a1bb9a81c9d6cbae5a6a395d803ca5664a4
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-21 02:13:47 +0530

    middleware fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 
+export const config = {
+    matcher: ["/sign-in", "/sign-up", "/", "/dashboard:path*", "/verify:path*"],
+};
+
 export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request });
     const url = request.nextUrl;
@@ -11,15 +15,12 @@ export async function middleware(request: NextRequest) {
         (url.pathname.startsWith("/sign-in") ||
             url.pathname.startsWith("/sign-up") ||
             url.pathname.startsWith("/verify") ||
-            url.pathname.startsWith("/"))
+            url.pathname === "/")
     ) {
         return NextResponse.redirect(new URL("/dashboard", request.url));
     }
     if (!token && url.pathname.startsWith("/dashboard")) {
         return NextResponse.redirect(new URL("/sign-in", request.url));
     }
+    return NextResponse.next();
 }
-
-export const config = {
-    matcher: ["/sign-in", "/sign-up", "/", "/dashboard:path*", "/verify:path*"],
-};
