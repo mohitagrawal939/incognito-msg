95aeeeef0d7be7fcf014f1d17123d9c772fc11a3
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-21 02:22:47 +0530

    public profile link page craeted

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const messageSchema = z.object({
     content: z
         .string({ required_error: "Message is required" })
-        .min(10, { message: "content must be at least of 10 characters" })
+        .min(10, { message: "Content must be at least of 10 characters" })
         .max(300, { message: "Content must be no longer than 300 characters" }),
 });
